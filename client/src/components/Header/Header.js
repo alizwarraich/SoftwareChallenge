b3f11e348172a766a16f4a2f7e95aa4cee11a3f8
Imran Ariffin <ariffinimran@gmail.com>
2018-06-03 14:10:29 -0400

    header twitter style

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -5,7 +5,24 @@ class Header extends React.Component {
   render() {
     return (
       <div style={styles.layout}>
-
+        <div style={styles.navItem}>
+          <a
+            style={styles.anchor}
+            href=''
+            onClick={e => e.preventDefault()}
+          >
+            Login
+          </a>
+        </div>
+        <div style={styles.navItem}>
+          <a
+            style={styles.anchor}
+            href=''
+            onClick={e => e.preventDefault()}
+          >
+            Signup
+          </a>
+        </div>
       </div>
     );
   }
--- client/src/components/Header/styles.js
+++ client/src/components/Header/styles.js
@@ -2,8 +2,22 @@ const layout = {
   height: '45px',
   width: '100%',
   background: '#1da1f2',
+  textAlign: 'right',
+};
+
+const navItem = {
+  float: 'right',
+  height: '100%',
+  minWidth: '50px',
+  padding: '10px 10px 5px 10px',
+};
+
+const anchor = {
+  color: 'white',
 };
 
 export default {
   layout,
+  navItem,
+  anchor,
 };
