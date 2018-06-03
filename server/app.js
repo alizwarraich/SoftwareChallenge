2aa85cfa5f00b6c8c3ade574613dc4c642a4f9c1
Imran Ariffin <ariffinimran@gmail.com>
2018-06-03 13:06:56 -0400

    enable cors

--- server/app.js
+++ server/app.js
@@ -11,6 +11,11 @@ var tweets = require('./routes/tweets');
 var app = express();
 
 app.use(logger('dev'));
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
